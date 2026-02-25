75543aaa3e4414bac7759f5abe6b992cb5de56a8
Edvaldojs22 <jonas.edvaldo.10@gmail.com>
2026-02-25 19:39:14 -0300

    feat: refactor ProductController and Product entity for improved service integration and validation

--- backend/src/main/java/com/autoflex/production/controller/ProductController.java
+++ backend/src/main/java/com/autoflex/production/controller/ProductController.java
@@ -2,44 +2,51 @@ package com.autoflex.production.controller;
 
 
 import com.autoflex.production.entity.Product;
-import com.autoflex.production.repository.ProductRepository;
 import com.autoflex.production.service.ProductionService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/products")
 @CrossOrigin("*")
 public class ProductController {
-    @Autowired
-    private ProductRepository repository;
+
     @Autowired
     private ProductionService productionService;
 
     @GetMapping
-    public List<Product> getAll() {
-        return repository.findAllByOrderByPriceDesc();
+    public ResponseEntity<List<Product>> getAll() {
+        List<Product> products = productionService.listAll();
+        return ResponseEntity.ok(products); // Status 200
     }
-
     @PostMapping
-    public Product create(@RequestBody Product product){
-
-        if(product.getMaterials() != null){
-            product.getMaterials().forEach(item -> item.setProduct(product) );
-        }
-        return  repository.save(product);
+    public ResponseEntity<Product> create(@Valid @RequestBody Product product) {
+        Product savedProduct = productionService.saveProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
     }
 
-    @GetMapping("{id}")
-    public  Product getById(@PathVariable Long id){
-        return  repository.findById(id).orElse(null);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productionService.delete(id);
+        return ResponseEntity.noContent().build(); // Status 204
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getById(@PathVariable Long id) {
+        Product product = productionService.findById(id);
+        return ResponseEntity.ok(product); // Status 200
     }
 
     @GetMapping("/suggest")
-    public List<String> getSuggestion(){
-        return  productionService.suggestProduction();
+    public ResponseEntity<List<Map<String, Object>>> getSuggestion() {
+        return ResponseEntity.ok(productionService.suggestProduction());
     }
-
 }
--- backend/src/main/java/com/autoflex/production/entity/Product.java
+++ backend/src/main/java/com/autoflex/production/entity/Product.java
@@ -1,7 +1,10 @@
 package com.autoflex.production.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -17,12 +20,12 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String code;
-
+    @NotBlank(message = "mandatory name")
     @Column(nullable = false)
     private String name;
 
+    @NotNull(message = "mandatory price")
+    @Min(value = 0, message = "The price cannot be negative")
     @Column(nullable = false)
     private Double price;
 
